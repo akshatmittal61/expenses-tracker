f9bafa31f49b36545eb633b496f4fc0ad535f8fc
akshatmittal61 <akshatmittal2506@gmail.com>
2021-12-02 00:33:47 +0530

    Added code for heroku

--- server.js
+++ server.js
@@ -46,5 +46,12 @@ app.delete('/delete/:id', (req, res) => {
     res.json(transactions);
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
